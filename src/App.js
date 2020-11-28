5a3861102844752e97442b78feecdb1887d5e814
naradanarya <52026986+naradanarya@users.noreply.github.com>
2020-11-29 00:16:41 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,32 +1,75 @@
-import React, { Component } from 'react';
+import React, { Fragment, useState, useEffect, } from 'react';
 import axios from 'axios';
-import ImageList from './imageList';
 
-class App extends Component {
-  state = { images: [] };
+ const useDataApi = (initialUrl, initialData) => {
+  const [data, setData] = useState(initialData);
+  const [url, setUrl] = useState(initialUrl);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
-  getPhotos = async () => {
-    const response = await axios.get("https://api.unsplash.com/photos", {
-      headers: {
-        Authorization: "Client-ID ENY3nA-GcEo3stBCPR5LGqSxwadLFLJCq74v1KMLOkQ"
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsError(false);
+      setIsLoading(true);
+
+      try {
+        const result = await axios(url);
+
+        setData(result.data);
+      } catch (error) {
+        setIsError(true);
       }
-    });
 
-    this.setState({ images: response.data});
-  }
+      setIsLoading(false);
+    };
 
-  render() {
-    return(
-      <div>
-        <input 
-          type="button" 
-          value='get photos'
-          onClick={this.getPhotos}
+    fetchData();
+  }, [url]);
+
+  return [{ data, isLoading, isError }, setUrl];
+};
+
+function App() {
+  const [query, setQuery] = useState('react');
+  const [{ data, isLoading, isError }, doFetch] = useDataApi(
+    'http://hn.algolia.com/api/v1/search?query=react',
+    { hits: [] },
+  );
+
+  return (
+    <Fragment>
+      <form
+        onSubmit={event => {
+          doFetch(
+            `http://hn.algolia.com/api/v1/search?query=${query}`,
+          );
+
+          event.preventDefault();
+        }}
+      >
+        <input
+          type="text"
+          value={query}
+          onChange={event => setQuery(event.target.value)}
         />
-        <ImageList  images={this.state.images} />
-      </div>
-    );
-  }
+        <button type="submit">Search</button>
+      </form>
+
+      {isError && <div>Something went wrong ...</div>}
+
+      {isLoading ? (
+        <div>Loading ...</div>
+      ) : (
+        <ul>
+          {data.hits.map(item => (
+            <li key={item.objectID}>
+              <a href={item.url}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
+      )}
+    </Fragment>
+  );
 }
 
 export default App;
