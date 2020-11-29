6734c575ee021dea1957fa6217e3348cdf9b835f
naradanarya <52026986+naradanarya@users.noreply.github.com>
2020-11-29 19:19:46 +0700

    new update

--- src/App.js
+++ src/App.js
@@ -3,13 +3,6 @@ import List from "./components/list";
 
 const App = () => {
   const [searchWord, setSearchWord] = useState("naruto");
-  // const [currentValue, setCurrentValue] = useState(initialValue)
-  // currentValue     : 使用する変数の名前
-  // setCurrentValue  : 使用する関数の名前
-  // useState         : Reactのメソッド
-  // initialValue     : stateにいれる最初の値
-
-  // 入力された値をstate保持させる関数
   const handleChange = e => {
     setSearchWord(e.target.value);
   };
@@ -19,7 +12,6 @@ const App = () => {
       <input
         type="text"
         placeholder="SearchWord"
-        //値が変わるたびにhandleChangeを動かす
         onChange={handleChange}
       />
       <List word={searchWord} />
